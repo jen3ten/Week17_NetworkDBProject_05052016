986f390af072a48a7abd6d1d1e74f33f93f00d4e
Jennifer Evans <jenevans3ten@gmail.com>
2016-05-08 21:24:11 -0400

    High Ratings View Page

--- Week17_NetworkDBProject_05052016/Buddy Classes/RatingAbove4Buddy.cs
+++ Week17_NetworkDBProject_05052016/Buddy Classes/RatingAbove4Buddy.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Week17_NetworkDBProject_05052016.Models
 {
-    [MetadataType(typeof(ShowBuddy))]
-    public partial class Show
+    [MetadataType(typeof(RatingAbove4Buddy))]
+    public partial class vwRatingsAbove4
     {
 
     }
 
-    sealed class ShowBuddy
+    sealed class RatingAbove4Buddy
     {
         public int ShowID { get; set; }
-        [Display(Name ="Network")]
         public int NetworkID { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
@@ -26,5 +26,4 @@ namespace Week17_NetworkDBProject_05052016.Models
         public string Description { get; set; }
 
     }
-
 }
